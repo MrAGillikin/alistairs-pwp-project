4c3720fda1583effff532933c618427f881521bf
Alistair Gillikin <agillikin1@cnm.edu>
2019-07-18 07:55:48 -0600

    Added Persona

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -1,1 +1,34 @@
 <?php
+
+echo "<p>Purpose: I would like my PWP to serve as a test for user-interactions.</p>";
+
+echo "<p>My audience consists of peers, as well as potential employers and clients.</p>";
+
+echo "<p>My goal is to make it impressive enough to gain a job or internship in coding.</p>";
+
+echo "<h1>User Persona:</h1>";
+echo		"<h2>Mitchell Hyne</h2>";
+echo		"<img src="businessman.jpg" alt="Mitchell Hyne" />";
+echo		"<ul>
+			<li>Age: 27</li>
+			<li>Tech Proficiency: Casual user</li>
+			<li>Occupation: Businessman</li>
+			<li>Machine: Dell Desktop running Windows 10. Google Chrome installed.</li>
+		</ul>
+		<h3>Likes</h3>
+		<ul>
+			<li>Getting things done</li>
+			<li>Smooth Design</li>
+			<li>Sharing info</li>
+			<li>Multi-tab browsing</li>
+		</ul>
+		<h3>Dislikes</h3>
+		<ul>
+			<li>Sudden schedule changes</li>
+			<li>Being rushed</li>
+			<li>Other people losing his work</li>
+			<li>Forced Updates</li>
+			<li>Losing passwords</li>
+		</ul>";
+
+>
