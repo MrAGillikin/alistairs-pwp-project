b32df0023c497b44b5dfa248d78a41291744dc0b
Alistair Gillikin <agillikin1@cnm.edu>
2019-08-01 06:50:42 -0600

    Milestone 2 basics done. Still need to add wireframe picture.

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8"/>
+	<title>PWP Milestone 2</title>
+</head>
+<body>
+
+<h1>Content Strategy:</h1>
+
+<p>My site will be one page, and consist of the following sections:</p>
+
+<p><strong>Welcome:</strong> At the top of the page will be a brief welcome to the website. It will mention my name, the name of the site, and a brief explanation of what it does. The title might be split into its own header.</p>
+
+<p><strong>Interface:</strong> Below that will be a section set aside to give the user options, and write feedback in response, using what I've learned during the bootcamp and in the Color Picker and Shopping List prework.</p>
+
+<p><strong>Contact:</strong> At the bottom will be a listing of who I am and how to contact me. This could be made a footer, but that might not be eyecatching enough. It will be tricky to make it visible without distracting from the Interface.</p>
+
+<p><strong>Background:</strong> The website's visual style should be simple, but appealing. I should limit it to two complementary colors as much as possible, with perhaps a third for things that should really stand out..</p>
+
+</body>
+</html>
