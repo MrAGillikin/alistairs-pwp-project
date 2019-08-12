996836e1e937b8eba20ebc769ffd7672df2d2665
Alistair Gillikin <agillikin1@cnm.edu>
2019-08-12 13:59:44 -0600

    Modified plans.

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -10,11 +10,11 @@
 
 <p>My site will be one page, and consist of the following sections:</p>
 
-<p><strong>Welcome:</strong> At the top of the page will be a brief welcome to the website. It will mention my name, the name of the site, and a brief explanation of what it does. The title might be split into its own header.</p>
+<p><strong>Welcome:</strong> At the top of the page will be a brief welcome to the website. It will mention my name, the name of the site, and a brief explanation of what it does and how to interact with it. The title might be split into its own header.</p>
 
 <p><strong>Interface:</strong> Below that will be a section set aside to give the user options, and write feedback in response, using what I've learned during the bootcamp and in the Color Picker and Shopping List prework.</p>
 
-<p><strong>Contact:</strong> At the bottom will be a listing of who I am and how to contact me. This could be made a footer, but that might not be eyecatching enough. It will be tricky to make it visible without distracting from the Interface.</p>
+<p><strong>Contact:</strong> At the bottom will be a listing of who I am, what I do, and how to contact me. That will include my credentials and core skills. A full contact form could go here, but it was not part of my original plans - or my original understanding of the project. This will be a footer, of one kind or another. It will be tricky to make it visible without distracting from the Interface.</p>
 
 <p><strong>Background:</strong> The website's visual style should be simple, but appealing. I should limit it to two complementary colors as much as possible, with perhaps a third for things that should really stand out..</p>
 
