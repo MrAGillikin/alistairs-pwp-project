58e81528a48d768719d1d09eaf7780458a980b13
Alistair Gillikin <agillikin1@cnm.edu>
2019-08-28 20:56:36 -0600

    Adjusting input form.

--- public_html/index.php
+++ public_html/index.php
@@ -35,7 +35,7 @@
 	<div class="jumbotron">
 		<h1 class="display-4">Entry Form</h1>
 		<p class="lead">This form takes a user input, examines it, and gives back a response. Currently, it is programmed to count the number of letters in each word.</p>
-		<form onsubmit="return myReplaceFunction()">
+		<form onsubmit="myReplaceFunction()">
 			Input<br>
 			<input type="text" name="Input" value="Enter"><br>
 			<input type="submit" value="Submit">
