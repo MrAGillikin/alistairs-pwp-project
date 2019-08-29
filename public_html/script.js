d9073057c38da302dafe1db4a1c972e1f07d4222
Alistair Gillikin <agillikin1@cnm.edu>
2019-08-29 01:38:03 -0600

    Adjusting function for readability and debugging.

--- public_html/script.js
+++ public_html/script.js
@@ -10,7 +10,7 @@ function myReplaceFunction() {
 
 function replaceWith(input){
 	console.log(input);
-	var arr = input.split(" "||", "||". "||"! "||"? ");
+	var arr = input.split(" ");
 	console.log(arr);
 	let updatedArr = arr.map(
 		(object) => {
